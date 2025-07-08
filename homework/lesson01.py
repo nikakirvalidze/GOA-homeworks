1698d24ae983a6b3349c579657d5c6d74880a419
nikakirvalidze <nikolozkirvalidze82@gmail.com>
2025-07-08 23:27:06 +0400

    second commit

---
+++ homework/lesson01.py
@@ -0,0 +1,15 @@
+country = "Georgia" 
+capital = "Tbilisi"
+invention = "vine"
+language = "georgian"
+
+
+Mom = "Natia"
+Dad = "Maxo"
+Grandpa = "Rezo"
+Grandma = "Nana"
+
+print (Grandma)
+print (Grandpa)
+print (Mom)
+print (Dad)
