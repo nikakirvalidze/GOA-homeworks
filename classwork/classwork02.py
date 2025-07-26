a845ae181a0e0a9cddc9cdbad22491f87b59bbc1
nikakirvalidze <nikolozkirvalidze82@gmail.com>
2025-07-26 23:18:13 +0400

    fourth commit

---
+++ classwork/classwork02.py
@@ -0,0 +1,26 @@
+number1 = 10 
+
+number2 = 5.5 
+
+print(number1 + number2)
+
+
+
+name = "nikolozi"
+age = "fourteen"
+print(name + age)
+
+print(5 + 3)    
+print(10 + 2.5) 
+
+print(9 - 4)    
+print(7.5 - 2)   
+
+print(6 * 3)  
+print(4.5 * 2)  
+
+print(10 / 2)   
+print(9 / 4)
+
+print(10 // 2)
+print( 9 // 4)
---
+++ homework/lesson03.py
@@ -0,0 +1,52 @@
+num1 = 10
+num2 = 20
+num3 = 30
+print( num1 + num2 + num3 )
+
+
+x = 15
+y = 20
+print( x + y )
+
+
+length = 20
+width = 10
+perimeter = 2 * (length + width)
+print(perimeter)
+
+
+print(2 + 3)
+print(10 + 7)
+print(0 + 8)
+print(25 + 30)
+print(100 + 200)
+
+print(5 - 2)
+print(10 - 7)
+print(8 - 0)
+print(30 - 25)
+print(100 - 60)
+
+print(2 * 3)
+print(4 * 5)
+print(0 * 100)
+print(6 * 7)
+print(10 * 10)
+
+print(6 / 3)
+print(10 / 5)
+print(100 / 10)
+print(20 / 4)
+print(8 / 2)
+
+print(7 // 3)
+print(10 // 4)
+print(15 // 5)
+print(9 // 2)
+print(12 // 6)
+
+print(7 / 3)
+print(10 / 4)
+print(15 / 5)
+print(9 / 2)
+print(12 / 7)
