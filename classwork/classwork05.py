7ebfe67584389ba44ddbd43ea30b64bcf77cc5bb
nikakirvalidze <nikolozkirvalidze82@gmail.com>
2025-08-07 01:07:00 +0400

    fifth commit

---
+++ classwork/classwork05.py
@@ -0,0 +1,28 @@
+# Debugging არის პროგრამაში შეცდომების აღმოჩენა და მათი გამოსწორება.
+# Case Sensitive ნიშნავს, რომ დიდ და პატარა ასოებს შორის განსხვავებაა.
+#python-ი არის case sensitive ენა, რადგან მასში ინგლისური დიდი ასოები განსხვავდება პატარებიდან.
+student_name = "ნიკოლოზი"
+student_age = 14
+academy_name = "გოა"
+pet_name = "როკო"
+favorite_subject = "მათემატიკა"
+print(student_name)
+print(student_age)
+print(academy_name)
+print(pet_name)
+print(favorite_subject)
+
+# 1numer = 10           შეცდომა: ცვლადი არ შეიძლება დაიწყოს ციფრით.
+# my-number = 25        შეცდომა: ცვლადის სახელში არ შეიძლება იყოს - ნიშანი.
+# student age = 14      შეცდომა: ცვლადის სახელში არ შეიძლება იყოს გამოტოვება.
+# 2number = 20          შეცდომა: ცვლადი არ შეიძლება დაიწყოს ციფრით
+# professor age = idk   შეცდომა: ცვლადის სახელში არ შეიძლება იყოს გამოტოვება
+
+number1 = 10
+number2 = 5
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 // num2)
+print(num1 % num2)
---
+++ homework/lesson05.py
@@ -0,0 +1,59 @@
+# კომენტარები გამოიყენება კოდის ახსნის მიზნით,  რათა კოდი უფრო გასაგები გახდეს თვითონ პროგრამისტისთვის ან
+#  სხვა ადამიანებისთვის, და მისი გამოყენება კარგი პრაქტიკაა ვინაიდან ეს ამარტივებს გუნდურ მუშაობა.
+
+parrot1_name = "როკო"
+parrot2_name = "როკო"
+my_name = "ნიკა"
+my_lastname = "კირვალიძე"
+my_age = 14
+my_favmovie = "pirates of carribian"
+my_favbook = "harry potter"
+my_favcar = "bmw m4"
+my_favcelebrity = "johnny depp"
+my_idk = "idk"
+print(parrot1_name)
+print(parrot2_name)
+print(my_name)
+print(my_lastname)
+print(my_age)
+print(my_favmovie)
+print(my_favbook)
+print(my_favcar)
+print(my_favcelebrity)
+print(my_idk)
+
+name = "Luka"
+age_num = 56
+my_favmov = "blood and bone"
+my_friends_name = "Nika"
+print("My name is" + "name")
+print("my age is" + "age num")
+print("My Favorite Movie is" + "my_favmov")
+print("My Friend's name is" + "my_friends_name")
+
+first_part = "მე მიყვარს "
+second_part = "პროგრამირება"
+sentence = first_part + second_part
+print(sentence)
+
+city = "თბილისი "
+country = "საქართველო"
+location = city + country
+print(location)
+
+name = "ნიკა"
+greeting = "გამარჯობა!"
+message = greeting + name
+print(message)
+
+str1 = "გილოცავ "
+str2 = "დაბადების დღეს!"
+result = str1 + str2
+print(result)
+
+legend = "technoblade"
+death_day = "never dies"
+Idk = legend + death_day
+print(Idk)
+
+# snake_case-არის underscore(_)-ის გამოყენება space-ის მაგივრად.
